7b35dab459b05e45a67068e11ff72c1481b04c4a
Владимир Синицын <vladimirsinicyn@MacBook-Pro-Vladimir.local>
2018-01-08 00:38:30 +0300

    code cleanup

--- stores/createStores.ts
+++ stores/createStores.ts
@@ -1,24 +1,15 @@
-import ApiStore from './ApiStore';
-import UserSettingsStore from './UserSettingsStore';
 import { observable } from 'mobx';
 import AppStore from './AppStore';
 
 export
 interface IStores {
-  api?;
-  userSettings?;
   app?;
-  navigator?;
 }
 
 function createStores(): IStores {
-  const api = new ApiStore();
-  const userSettings = new UserSettingsStore(api);
-  const app = new AppStore(userSettings, api);
+  const app = new AppStore();
 
   const s = observable({
-    api,
-    userSettings,
     app,
   }) as IStores;
 
--- utils/sleep.ts
+++
@@ -1,4 +0,0 @@
-export default
-function sleep(time) {
-    return new Promise(res => setTimeout(res, time));
-}
